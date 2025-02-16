1b4904ef59f7647b34a94514b58efe6c76206091
MahdaArtem-py <artm.magda@gmail.com>
2025-02-16 12:10:57 +0200

    Refactored Code with OOP & SOLID Principles

--- scraper.py
+++ scraper.py
@@ -4,38 +4,67 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 import time
 
-driver = webdriver.Chrome()
-site_to_scrape  = "https://jobs.dou.ua/vacancies/?category=Python"
-driver.get(site_to_scrape)
 
-work_data = []
+class JobScraper:
+    """Scrape list of python vacancies from Dou"""
 
-def scrape_jobs():
-    job_container = driver.find_elements(By.CLASS_NAME, "l-vacancy")
-    for job in job_container:
-        position = job.find_element(By.CLASS_NAME, "vt").text
-        company = job.find_element(By.CLASS_NAME, "company").text
-        city = job.find_element(By.CLASS_NAME, "cities").text
-        try:
-            salary = job.find_element(By.CLASS_NAME, "salary").text
-        except:
-            salary = None
-        work_data.append({"position": position, "company": company, "city or remote": city, "salary": salary})
+    def __init__(self) -> None:
+        self.url = "https://jobs.dou.ua/vacancies/?category=Python"
+        self.driver = webdriver.Chrome()
+        self.jobs = []
 
-scrape_jobs()
+    def open_site(self) -> None:
+        """Open Jobs Dou with python vacancies"""
 
-while True:
-    try:
-        load_more_button = driver.find_element(By.CSS_SELECTOR, ".more-btn a")
-        actions = ActionChains(driver)
-        actions.move_to_element(load_more_button).perform()
-        load_more_button.click()
-        time.sleep(2)
-        scrape_jobs()
-    except:
-        print("No more jobs")
-        break
+        self.driver.get(self.url)
 
-df = pd.DataFrame(work_data)
-df.to_csv("jobs.csv", index=False)
-driver.quit()
+    def scrape_jobs(self) -> None:
+        """Extract Python vacancies from Dou"""
+
+        job_container = self.driver.find_elements(By.CLASS_NAME, "l-vacancy")
+        for job in job_container:
+            position = job.find_element(By.CLASS_NAME, "vt").text
+            company = job.find_element(By.CLASS_NAME, "company").text
+            city = job.find_element(By.CLASS_NAME, "cities").text
+            try:
+                salary = job.find_element(By.CLASS_NAME, "salary").text
+            except:
+                salary = None
+            self.jobs.append({"position": position,
+                              "company": company,
+                              "city or remote": city,
+                              "salary": salary})
+
+    def load_more_btn(self) -> None:
+        """Click on Load More button"""
+        while True:
+            try:
+                load_more_button = self.driver.find_element(By.CSS_SELECTOR,
+                                                            ".more-btn a")
+                actions = ActionChains(self.driver)
+                actions.move_to_element(load_more_button).perform()
+                load_more_button.click()
+                time.sleep(2)
+                self.scrape_jobs()
+            except:
+                print("No more jobs")
+                break
+
+    def save_to_csv(self, filename: str) -> None:
+        """Save Python vacancies to CSV"""
+        df = pd.DataFrame(self.jobs)
+        df.to_csv(filename, index=False)
+        print(f"Saved data to {filename}.csv")
+
+    def run_scraping(self) -> None:
+        """Run scraping jobs"""
+        self.open_site()
+        self.scrape_jobs()
+        self.load_more_btn()
+        self.save_to_csv("jobs.csv")
+        print("Finished")
+
+
+if __name__ == "__main__":
+    scraper = JobScraper()
+    scraper.run_scraping()
