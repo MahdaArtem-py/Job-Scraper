e03e14155888b944abe033512e0c79c8cc9e881b
MahdaArtem-py <artm.magda@gmail.com>
2025-02-16 13:21:56 +0200

    Added category choosing for scrape

--- scraper.py
+++ scraper.py
@@ -4,22 +4,55 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 import time
 
+from selenium.webdriver.support.select import Select
+
 
 class JobScraper:
-    """Scrape list of python vacancies from Dou"""
+    """Scrape list of vacancies from Dou"""
 
     def __init__(self) -> None:
-        self.url = "https://jobs.dou.ua/vacancies/?category=Python"
+        self.BASE_URL = "https://jobs.dou.ua/"
         self.driver = webdriver.Chrome()
         self.jobs = []
 
     def open_site(self) -> None:
-        """Open Jobs Dou with python vacancies"""
+        """Open Jobs Dou with vacancies"""
 
-        self.driver.get(self.url)
+        self.driver.get(self.BASE_URL)
+
+    def get_categories(self) -> tuple:
+        """Extracts job categories from the dropdown menu"""
+
+        self.open_site()
+        time.sleep(2)
+        categories_dropdown = self.driver.find_element(By.NAME, "category")
+        select = Select(categories_dropdown)
+        categories = {i: option.text for i, option
+                      in enumerate(select.options, start=1)}
+        print("\nAvailable Categories:")
+        for index, name in categories.items():
+            print(f"{index}. {name}")
+        return select, categories
+
+    def chose_category(self):
+        """Choose category from dropdown menu in terminal"""
+
+        select, categories = self.get_categories()
+
+        while True:
+            try:
+                choice = int(input("\nChoose a category: "))
+                if choice in categories:
+                    select.select_by_visible_text(categories[choice])
+                    time.sleep(2)
+                    return
+                else:
+                    print("\nInvalid choice. Please try again.")
+            except ValueError:
+                print("Invalid input. Enter a number.")
 
     def scrape_jobs(self) -> None:
-        """Extract Python vacancies from Dou"""
+        """Extract vacancies from Dou"""
 
         job_container = self.driver.find_elements(By.CLASS_NAME, "l-vacancy")
         for job in job_container:
@@ -51,7 +84,7 @@ class JobScraper:
                 break
 
     def save_to_csv(self, filename: str) -> None:
-        """Save Python vacancies to CSV"""
+        """Save vacancies to CSV"""
         df = pd.DataFrame(self.jobs)
         df.to_csv(filename, index=False)
         print(f"Saved data to {filename}.csv")
@@ -59,6 +92,7 @@ class JobScraper:
     def run_scraping(self) -> None:
         """Run scraping jobs"""
         self.open_site()
+        self.chose_category()
         self.scrape_jobs()
         self.load_more_btn()
         self.save_to_csv("jobs.csv")
